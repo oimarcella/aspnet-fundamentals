10dd780b0ac1186e1bb6aaebb5f9070fc0fc6cc3
Marcella <marcella.amorimsa@gmail.com>
2024-07-23 19:53:02 -0300

    doc: comentando BlogDataContext

--- Data/BlogDataContext.cs
+++ Data/BlogDataContext.cs
@@ -6,12 +6,15 @@ namespace Blog.Data
 {
     public class BlogDataContext : DbContext
     {
+        //Tabelas referenciadas
         public DbSet<Category> Categories { get; set; }
         public DbSet<Post> Posts { get; set; }
         public DbSet<User> Users { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;");
 
+        //Aplicando FluntMapping
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new CategoryMap());

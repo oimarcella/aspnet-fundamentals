c2d6dd3d7d85113b899a6bda88ac006a86a4a8ab
Marcella <marcella.amorimsa@gmail.com>
2024-07-23 19:51:47 -0300

    Injetando tokenService de forma menos verbosa

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -5,13 +5,9 @@ using Microsoft.AspNetCore.Mvc;
 namespace Blog.Controllers;
 public class AccountController : ControllerBase
 {
-    private readonly TokenService _tokenService;
-    public AccountController(TokenService tokenService) {
-        _tokenService = tokenService;
-    }
     [HttpPost("v1/login")]
-    public IActionResult Login() {
-        var token = _tokenService.GenerateToken(null);
+    public IActionResult Login([FromServices] TokenService tokenService) {
+        var token = tokenService.GenerateToken(null);
         return Ok(new ResultViewModel<TokenService>(token));
     }
 }

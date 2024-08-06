63bcca096db6a53001f2d141235d21192c14c3bf
Marcella <marcella.amorimsa@gmail.com>
2024-08-06 12:10:47 -0300

    feat: cria attribute(notation) para validar acesso de apikey

---
+++ Attributes/ApiKeyAttribute.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Threading.Tasks;
+
+namespace Blog.Attributes
+{
+    //Para poder decorar uma classe        ou               um método
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class ApiKeyAttribute : Attribute, IActionFilter
+    {
+        public void OnActionExecuting(
+        ActionExecutingContext context)
+        {
+            if (!context.HttpContext.Request.Query.TryGetValue(Configuration.ApiKeyName, out var extractedApiKey))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "ApiKey não encontrada"
+                };
+                return;
+            }
+
+            if (!Configuration.ApiKeyValue.Equals(extractedApiKey))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 403,
+                    Content = "Acesso não autorizado"
+                };
+                return;
+            }
+
+            // Execução continua automaticamente sem o next() chamado explicitamente
+
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            // Este método pode ser deixado vazio se não houver necessidade de executar lógica após a execução da ação
+        }
+    }
+}
--- Configuration.cs
+++ Configuration.cs
@@ -1,5 +1,16 @@
 ﻿namespace Blog;
 public static class Configuration
 {
-    public static string JwtKey { get; set; } = "RozOU+mjqkaZAvilStv+Uw==RozOU+mjqkaZAvilStv+Uw==";
+    public static string JwtKey = "RozOU+mjqkaZAvilStv+Uw==RozOU+mjqkaZAvilStv+Uw==";
+    public static string ApiKeyName = "api_key";
+    public static string ApiKeyValue = "curso_api_a3343faf-acc4-4e2e-9dd1-e2f0e972ef3e";
+    public static SmtpConfiguration Smtp = new();
+
+    public class SmtpConfiguration
+    {
+        public string Host{ get; set; }
+        public int Port { get; set; } = 25;
+        public string UserName{ get; set; }
+        public string Password { get; set; }
+    }
 }

a4c4348d8e9058c2b2c35944f5cc95c7b57e495a
Marcella <marcella.amorimsa@gmail.com>
2024-08-07 14:58:05 -0300

    paginar api de get posts

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -12,7 +12,9 @@ public class PostController : ControllerBase
 {
     [HttpGet("v1/posts")]
     public async Task<IActionResult> GetAsync(
-        [FromServices] BlogDataContext context
+        [FromServices] BlogDataContext context,
+        [FromQuery] int page = 0,
+        [FromQuery] int pageSize = 0
     )
     {
         var posts = await context.Posts
@@ -27,7 +29,10 @@ public class PostController : ControllerBase
             Category = x.Category.Name,
             Author = $"{x.Author.Name} ({x.Author.Email})"
         })
+        .Skip(page * pageSize)
+        .Take(pageSize)
         .ToArrayAsync();
+
         return Ok(posts);
     }
 }
